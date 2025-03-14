c0602adbd1dda179df84308774137ad159c367ce
doctornemesis <amarakash1.aa@gmail.com>
2025-03-14 18:58:59 +0530

    Updated build config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Disable ESLint during production builds
+    output: 'export',
     eslint: {
         // Warning: this will skip checking ESLint errors during the build
         ignoreDuringBuilds: true,
